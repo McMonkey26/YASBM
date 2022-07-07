650e0c81fe916c49b51adb49c2854ffaa8f87a83
Steve Scruff <mutantpiggiegolems@gmail.com>
2022-07-06 18:21:38 -0700

    first commir

--- src/main/java/com/pew/yetanotherskyblockmod/mixin/ExampleMixin.java
+++ src/main/java/com/pew/yetanotherskyblockmod/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package com.pew.yetanotherskyblockmod.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import com.pew.yetanotherskyblockmod.YASBM;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		YASBM.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
